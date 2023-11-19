9cd70314ebd2612c02fc06b14c728ab7e16d23e3
aby <anti1346@gmail.com>
2023-11-19 23:28:19 +0900

    no message

--- create_instance.sh
+++ create_instance.sh
@@ -11,17 +11,16 @@ if [ -f .env ]; then
   echo "환경 변수가 로드되었습니다."
 else
   echo ".env 파일을 찾을 수 없습니다."
-  exit 1
+  # 기본 값 설정
+  ami_id=${1:-ami-0}
+  instance_name=${2:-instance}
+  instance_count=${3:-1}
+  instance_type=${4:-t4g.medium}
+  key_name=${5:-keyfile}
+  subnet_id=${subnet_id:-your_subnet_id}  # your_subnet_id에 실제 서브넷 ID를 지정
+  security_groups=${security_groups:-your_security_groups}  # your_security_groups에 실제 보안 그룹 ID를 지정
+  #exit 1
 fi
-
-# 기본 값 설정
-ami_id=${1:-ami-0}
-instance_name=${2:-instance}
-instance_count=${3:-1}
-instance_type=${4:-t4g.medium}
-key_name=${5:-keyfile}
-subnet_id=${subnet_id:-your_subnet_id}  # your_subnet_id에 실제 서브넷 ID를 지정
-security_groups=${security_groups:-your_security_groups}  # your_security_groups에 실제 보안 그룹 ID를 지정
 
 # AWS CLI를 사용하여 EC2 인스턴스 생성
 aws --region ap-northeast-2 ec2 run-instances \
